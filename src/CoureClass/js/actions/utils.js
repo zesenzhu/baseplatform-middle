39c628de99c0a6213c75547c3d984302ccb31799
MKJS <2362039373@qq.com>
2020-12-25 11:25:30 +0800

    ''

--- src/CoureClass/js/actions/utils.js
+++ src/CoureClass/js/actions/utils.js
@@ -1,238 +1,220 @@
-import {getData,postData} from "../../../common/js/fetch";
+import { getData, postData } from "../../../common/js/fetch";
 
-import CONFIG from '../../../common/js/config';
+import CONFIG from "../../../common/js/config";
 
-import upUIState from './UpUIState';
+import upUIState from "./UpUIState";
 
 const Api = CONFIG.CourseClassProxy;
 
 //获取数据以及封装数据格式
-export const getGetData =  async (url,level,api=Api,mode='cors',arr=[500,401,403]) =>{
+export const getGetData = async (
+  url,
+  level,
+  api = Api,
+  mode = "cors",
+  arr = [500, 401, 403]
+) => {
+  try {
+    let fetchAsync = "";
 
     try {
-
-        let fetchAsync = '';
-
-        try {
-
-            fetchAsync = await getData(api+url,level,mode,true,arr);
-
-        }
-        catch (e) {
-
-            return  e;
-
-        }
-
-        let json = await fetchAsync.json();
-
-        return json;
-
+      fetchAsync = await getData(api + url, level, mode, true, arr);
+    } catch (e) {
+      return e;
     }
-    catch (e) {
 
-        return e;
+    let json = await fetchAsync.json();
 
-    }
+    return json;
+  } catch (e) {
+    return e;
+  }
 };
 //调用post接口
-export const getPostData = async (url,data,level,api=Api,content_type='json',arr=[500,401,403]) =>{
+export const getPostData = async (
+  url,
+  data,
+  level,
+  api = Api,
+  content_type = "json",
+  arr = [500, 401, 403]
+) => {
+  try {
+    let fetchAsync = "";
 
     try {
-        let fetchAsync = '';
-
-        try {
-
-            fetchAsync = await postData(api+url,data,level,content_type,true,arr);
-
-        }
-        catch (e) {
-            return  e;
-        }
-
-        let json = await fetchAsync.json();
-
-        return  json;
-
-    }
-    catch (e) {
-
-        return e;
-
+      fetchAsync = await postData(
+        api + url,
+        data,
+        level,
+        content_type,
+        true,
+        arr
+      );
+    } catch (e) {
+      return e;
     }
 
+    let json = await fetchAsync.json();
+
+    return json;
+  } catch (e) {
+    return e;
+  }
 };
 
+export const showErrorAlert = ({
+  title,
+  littleTitle,
+  ok,
+  cancel,
+  close,
+  okShow,
+  cancelShow,
+  okTitle,
+  cancelTitle,
+}) => {
+  return (dispatch) => {
+    dispatch({
+      type: upUIState.SHOW_ERROR_ALERT,
 
-export const showErrorAlert = ({title,littleTitle,ok,cancel,close,okShow,cancelShow,okTitle,cancelTitle}) => {
+      msg: {
+        type: "btn-error",
 
-    return dispatch=>{
+        title,
 
-        dispatch({
+        littleTitle,
 
-            type:upUIState.SHOW_ERROR_ALERT,
+        ok: ok ? ok : hideAlert(dispatch),
 
-            msg:{
+        cancel: cancel ? cancel : hideAlert(dispatch),
 
-                type:'btn-error',
+        close: close ? close : hideAlert(dispatch),
 
-                title,
+        okTitle,
 
-                littleTitle,
-
-                ok:ok?ok:hideAlert(dispatch),
-
-                cancel:cancel?cancel:hideAlert(dispatch),
-
-                close:close?close:hideAlert(dispatch),
-
-                okTitle,
-
-                cancelTitle
-
-            }
-
-        })
-
-    }
-
+        cancelTitle,
+      },
+    });
+  };
 };
 
-export const showWarnAlert = ({title,littleTitle,ok,cancel,close,okShow,cancelShow,okTitle,cancelTitle}) => {
+export const showWarnAlert = ({
+  title,
+  littleTitle,
+  ok,
+  cancel,
+  close,
+  okShow,
+  cancelShow,
+  okTitle,
+  cancelTitle,
+}) => {
+  return (dispatch) => {
+    dispatch({
+      type: upUIState.SHOW_ERROR_ALERT,
 
-    return dispatch=>{
+      msg: {
+        type: "btn-warn",
 
-        dispatch({
+        title,
 
-            type:upUIState.SHOW_ERROR_ALERT,
+        littleTitle,
 
-            msg:{
+        ok: ok ? ok : hideAlert(dispatch),
 
-                type:'btn-warn',
+        cancel: cancel ? cancel : hideAlert(dispatch),
 
-                title,
+        close: close ? close : hideAlert(dispatch),
 
-                littleTitle,
+        okTitle,
 
-                ok:ok?ok:hideAlert(dispatch),
-
-                cancel:cancel?cancel:hideAlert(dispatch),
-
-                close:close?close:hideAlert(dispatch),
-
-                okTitle,
-
-                cancelTitle
-
-            }
-
-        })
-
-    }
-
+        cancelTitle,
+      },
+    });
+  };
 };
 
-export const showSuccessAlert = ({title,hide}) => {
+export const showSuccessAlert = ({ title, hide }) => {
+  return (dispatch) => {
+    dispatch({
+      type: upUIState.SHOW_ERROR_ALERT,
 
-    return dispatch=>{
+      msg: {
+        type: "success",
 
-        dispatch({
+        title,
 
-            type:upUIState.SHOW_ERROR_ALERT,
-
-            msg:{
-
-                type:'success',
-
-                title,
-
-                onHide:hide?hide:hideAlert(dispatch)
-
-            }
-
-        })
-
-    }
-
+        onHide: hide ? hide : hideAlert(dispatch),
+      },
+    });
+  };
 };
 
-export const showQueryAlert = ({title,littleTitle,ok,cancel,close,okShow,cancelShow,okTitle,cancelTitle}) => {
+export const showQueryAlert = ({
+  title,
+  littleTitle,
+  ok,
+  cancel,
+  close,
+  okShow,
+  cancelShow,
+  okTitle,
+  cancelTitle,
+}) => {
+  return (dispatch) => {
+    dispatch({
+      type: upUIState.SHOW_ERROR_ALERT,
 
-    return dispatch=>{
+      msg: {
+        type: "btn-query",
 
-        dispatch({
+        title,
 
-            type:upUIState.SHOW_ERROR_ALERT,
+        littleTitle,
 
-            msg:{
+        ok: ok ? ok : hideAlert(dispatch),
 
-                type:'btn-query',
+        cancel: cancel ? cancel : hideAlert(dispatch),
 
-                title,
+        close: close ? close : hideAlert(dispatch),
 
-                littleTitle,
+        okShow: okShow === "n" ? "n" : "y",
 
-                ok:ok?ok:hideAlert(dispatch),
+        cancelShow: cancelShow === "n" ? "n" : "y",
 
-                cancel:cancel?cancel:hideAlert(dispatch),
+        okTitle,
 
-                close:close?close:hideAlert(dispatch),
-
-                okShow:okShow==='n'?'n':'y',
-
-                cancelShow:cancelShow==='n'?'n':'y',
-
-                okTitle,
-
-                cancelTitle
-
-            }
-
-        })
-
-    }
-
+        cancelTitle,
+      },
+    });
+  };
 };
 
-
-
-export const hideAlert = (dispatch) =>{
-
-    return e=>dispatch({type:upUIState.CLOSE_ERROR_ALERT});
-
+export const hideAlert = (dispatch) => {
+  return (e) => dispatch({ type: upUIState.CLOSE_ERROR_ALERT });
 };
-
 
 //学科名称正则
-export const subNameReg = (str)=>{
+export const subNameReg = (str) => {
+  const Reg = /^[,_\->/()（）A-Za-z0-9\u4e00-\u9fa5]{1,50}$/;
 
-    const Reg = /^[,_\->/()（）A-Za-z0-9\u4e00-\u9fa5]{1,50}$/;
-
-    return Reg.test(str);
-
+  return Reg.test(str);
 };
-
 
 //学科编号正则
 
-export const subNumReg = (str) =>{
+export const subNumReg = (str) => {
+  const Reg = /^[0-9]*$/;
 
-    const Reg = /^[0-9]*$/;
-
-    return Reg.test(str);
-
+  return Reg.test(str);
 };
-
-
-
 
 //人物名称正则
 export const teacherSearchReg = (str) => {
+  let pattern = "";
 
-    let pattern = '';
-
-    pattern =  /^[A-Za-z0-9]{1,30}$|^[a-zA-Z0-9_.·\u4e00-\u9fa5 ]{0,48}[a-zA-Z0-9_.·\u4e00-\u9fa5]$/
-
-    return pattern.test(str);
-
+  // pattern =  /^[A-Za-z0-9]{1,30}$|^[a-zA-Z0-9_.·\u4e00-\u9fa5 ]{0,48}[a-zA-Z0-9_.·\u4e00-\u9fa5]$/   //旧的不支持（）
+  pattern = /^[_\->/()（）A-Za-z0-9\u4e00-\u9fa5]{0,50}$|^[A-Za-z0-9]{1,30}$|^[a-zA-Z0-9_.·\u4e00-\u9fa5 ]{0,48}[a-zA-Z0-9_.·\u4e00-\u9fa5]$/;
+  return pattern.test(str);
 };
